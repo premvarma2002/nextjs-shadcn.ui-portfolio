d270a55884ad8ba4ad811038c5cb0d0c55e348fc
premvarma2002 <vakilverma1982@gmail.com>
2023-12-07 11:52:38 +0530

    Navbar shadow

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,31 +1,54 @@
-'use client'
+"use client";
 
-import { useState ,useEffect } from 'react'
-// components 
-import ThemeToggler from './ThemeToggler'
-import Logo from './Logo'
-import Nav from './Nav'
-import MobileNav from './MobileNav'
+import { useState, useEffect } from "react";
+// components
+import ThemeToggler from "./ThemeToggler";
+import Logo from "./Logo";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+import { usePathname } from "next/navigation";
 
 const header = () => {
+  const [header, setHeader] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const scrollYPos = window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    });
+
+    // remove event
+    return () => window.removeEventListener("scroll", scrollYPos);
+  });
+
   return (
-    <header>
+    <header
+      className={`${
+        header
+         ? "py-4 bg-white shadow-lg  dark:bg-accent" 
+         : "py-6 dark:bg-transparent"
+      } sticky top-0 z-30 transition-all`}
+    >
       <div className="container mx-auto">
-      <div className="flex justify-between items-center">
-          <Logo/>
-          <div className='flex items-center gap-x-6'>
-           {/* {nav} */}
-           <Nav containerStyles='hidden xl:flex gap-x-8 items-center' linkStyles='relative hover:text-primary transition-all' underlineStyles='absolute left-0 top-full h-[2px] bg-primary w-full'/>
-        <ThemeToggler/>
-        {/* {mobile nav} */}
-        <div className='xl:hidden'>
-          <MobileNav/>
-        </div>
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div className="flex items-center gap-x-6">
+            {/* {nav} */}
+            <Nav
+              containerStyles="hidden xl:flex gap-x-8 items-center"
+              linkStyles="relative hover:text-primary transition-all"
+              underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"
+            />
+            <ThemeToggler />
+            {/* {mobile nav} */}
+            <div className="xl:hidden">
+              <MobileNav />
+            </div>
           </div>
-      </div>
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default header
+export default header;
