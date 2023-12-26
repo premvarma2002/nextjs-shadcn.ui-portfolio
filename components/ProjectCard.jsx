0b8f27a57bb5e93fc5fe1ac6cf6273fac3eb4a80
premvarma2002 <vakilverma1982@gmail.com>
2023-12-26 15:27:47 +0530

    badge import bug fixed

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Card, CardHeader } from "./ui/card";
 import { Github, Link2Icon } from "lucide-react";
-import { Badge } from "./ui/Badge";
+import { Badge } from "./ui/badge";
 
 const ProjectCard = ({ project }) => {
   return (
