4c23674d1f265e4fcbc2ed35c35635caa583516b
premvarma2002 <vakilverma1982@gmail.com>
2023-12-29 13:49:00 +0530

    formspree append for form functionality

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,32 +1,38 @@
-'use client'
+"use client";
 
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
-import { User, MailIcon, ArrowRightIcon, MessageSquare } from 'lucide-react';
+import { User, MailIcon, ArrowRightIcon, MessageSquare } from "lucide-react";
 const Form = () => {
-
-
   return (
-
-    <form className="flex flex-col gap-y-4" action="https://getform.io/f/73e2e40a-f072-452a-b315-080c295cb940" method="POST">
+    // Now using formspree here .. before i used getform.io  ( please dont copy my form action link.. it wont work for you)
+    <form
+      className="flex flex-col gap-y-4"
+      action="https://formspree.io/f/moqgqqng"
+      method="POST"
+    >
       {/* input  */}
       <div className="relative flex items-center">
-        <Input type='name' id='name' name='name' placeholder='Name'/>
-        <User className="absolute right-6" size={20}/>
+        <Input type="name" id="name" name="name" placeholder="Name" />
+        <User className="absolute right-6" size={20} />
       </div>
       {/* input  */}
       <div className="relative flex items-center">
-        <Input type='email' id='email' name='email' placeholder='Email'/>
-        <MailIcon className="absolute right-6" size={20}/>
+        <Input type="email" id="email" name="email" placeholder="Email" />
+        <MailIcon className="absolute right-6" size={20} />
       </div>
       {/* input  */}
       <div className="relative flex items-center">
-        <Textarea id='message' name='message' placeholder='Type your message here.'/>
-        <MessageSquare className="absolute top-4 right-6" size={20}/>
+        <Textarea
+          id="message"
+          name="message"
+          placeholder="Type your message here."
+        />
+        <MessageSquare className="absolute top-4 right-6" size={20} />
       </div>
-      <Button type="submit" className='flex items-center gap-x-1 max-w-[166px]' >
-        Let's Talk <ArrowRightIcon size={20}/>
+      <Button type="submit" className="flex items-center gap-x-1 max-w-[166px]">
+        Let's Talk <ArrowRightIcon size={20} />
       </Button>
     </form>
   );
